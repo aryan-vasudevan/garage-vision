b0b3778cffc0eced988c5cb9e7d5b66440b8c689
Aryan Vasudevan <vasudevan.aryan@gmail.com>
2026-06-10 15:12:24 -0400

    Initial Commit

---
+++ garage-vision/garage_visionApp.swift
@@ -0,0 +1,17 @@
+//
+//  garage_visionApp.swift
+//  garage-vision
+//
+//  Created by Aryan Vasudevan on 2026-06-10.
+//
+
+import SwiftUI
+
+@main
+struct garage_visionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
